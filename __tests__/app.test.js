0c981326dfeb1a7f218c0a0e05efd4b7e9aa147a
RichRavenM <richard.s.morris@btinternet.com>
2023-08-14 13:04:42 +0100

    adds merged changes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -48,6 +48,48 @@ describe("generic invalid URL error", () => {
   });
 });
 
+describe("/api", () => {
+  test("GET: 200: responds with object containing all the api endpoints", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then((response) => {
+        const { endpoints } = response.body;
+        expect(endpoints).toEqual({
+          "GET /api": {
+            description:
+              "serves up a json representation of all the available endpoints of the api",
+          },
+          "GET /api/topics": {
+            description: "serves an array of all topics",
+            queries: [],
+            exampleResponse: {
+              topics: [{ slug: "football", description: "Footie!" }],
+            },
+          },
+          "GET /api/articles": {
+            description: "serves an array of all articles",
+            queries: ["author", "topic", "sort_by", "order"],
+            exampleResponse: {
+              articles: [
+                {
+                  title: "Seafood substitutions are increasing",
+                  topic: "cooking",
+                  author: "weegembump",
+                  body: "Text from the article..",
+                  created_at: "2018-05-30T15:59:13.341Z",
+                  votes: 0,
+                  comment_count: 6,
+                },
+              ],
+            },
+          },
+        });
+      });
+  });
+});
+
+
 describe("/api/articles/:article_id", () => {
   describe("GET", () => {
     test("200: responds with a single article and contains all the relevant information for it", () => {
